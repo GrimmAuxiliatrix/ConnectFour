e8a5086b0339e46a359310c32c0f1caaf1945fea
LadenPopsicle <mullingt@bxscience.edu>
2017-03-13 20:13:34 -0400

    Update ConnectFour.java

--- ConnectFour.java
+++ ConnectFour.java
@@ -34,19 +34,82 @@ public class ConnectFour {
     return board;
    }
    
+public class ConnectFour {
+  
+   public static char[][] displayBoard(char[][] board) {         //displays board
+    for(int row=0; row < board.length; row++) {
+     for(int col=0; col < board[row].length; col++) {
+        System.out.print(board[row][col] + " ");
+      }
+      System.out.println("");
+   }
+    return board;
+  }
+   
+   public static char[][] makeBoard(char[][] board){           //makes initial board
+      for(int row=0; row<board.length; row++) {
+        for(int col=0; col<board[row].length; col++) {
+         board[row][col] = 'O';
+        }
+      }
+      return board;
+    }
+   
+   public static char[][] setPiece(char[][] board, int r, int c) {          //player sets red piece
+     while(board[r+1][c] =='O') {
+      r++;
+     }
+      board[r][c] = 'R';
+     
+    /* for(int i=0; i<6; i++) {
+       if(board[r+1][c] !='O') {
+        board[r][c] = 'R';
+       }
+     }    */
+    return board;
+   }
+   
+   
    public static boolean winner(char[][] board) {
-     int counter = 1;
-     for(int i=0; i<board.length; i++) {
-       for(int j=0; j<board[i].length; j++) {
-         if(board[i][j] == board[i+counter][j+counter]) {
-          System.out.println("YOU'RE WINNER!");
+     //win vertically
+     for(int row = 0; row<= board.length-4; row++) {
+       for(int col = 0; col<board[0].length; col++) {
+         if(board[row][col] !=0 && board[row][col]==board[row+1][col] && board[row+1][col] == board[row+2][col] && board[row+2][col]==board[row+3][col]) {
           return true;
          }
        }
-       counter++;
      }
-  return false;
+ //win diagonally downward
+  for(int row = 0; row<= board.length-4; row++) {
+       for(int col = 0; col<=board[0].length-4; col++) {
+         if(board[row][col] !=0 && board[row][col]==board[row+1][col+1] && board[row+2][col+2] == board[row+1][col+1] && board[row+3][col+3]==board[row+2][col+2]) {
+          return true;
+         }
+       }
+     }
+  //win horizontally
+ for(int row = 0; row< board.length; row++) {
+       for(int col = 0; col<=board[0].length-4; col++) {
+         if(board[row][col] !=0 && board[row][col]==board[row][col+1] && board[row][col+1] == board[row][col+2] && board[row][col+2]==board[row][col+3]) {
+          return true;
+         }
+       }
+     }
+ //win diagonally upward
+ 
+ for(int row = 3; row< board.length; row++) {
+       for(int col = 0; col<=board[0].length-4; col++) {
+         if(board[row][col] !=0 && board[row][col]==board[row-1][col+1] && board[row-1][col+1] == board[row-2][col+2] && board[row-2][col+2]==board[row-3][col+3]) {
+          return true;
+         }
+       }
+     }
+ return false;
 }
+}
+  
+  
+ 
 }
   
   
