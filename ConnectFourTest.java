257a2b346ef7a24bb0120c6a22cee16d90eebb04
GrimmAuxiliatrix <ujala601@gmail.com>
2017-03-16 02:29:13 -0700

    Update ConnectFourTest.java

--- ConnectFourTest.java
+++ ConnectFourTest.java
@@ -3,25 +3,22 @@ import java.util.Scanner;
 public class ConnectFourTest{
   
   public static void main(String[] args){
-     ConnectFour connectFour = new ConnectFour();
+     ConnectFour connect = new ConnectFour(); // I changed it to connect because connectFour was too long
      Scanner tu = new Scanner(System.in);
-     int numRows = 6;
-     int numCols = 7;
-     char[][] board = new char[6][7];
-     connectFour.displayBoard(connectFour.makeBoard(board));
+     char[][] board = new char[connect.getRows()][connect.getCols()];
+     connect.displayBoard(connect.makeBoard(board));
      
      boolean gameIsOver = false;
-      
+     
      while(!gameIsOver){
        
-     System.out.println("Enter the row where you want to make your move.");
-     int row = tu.nextInt()-1;
-     System.out.println("Enter the column where you want to make your move.");
-     int column = tu.nextInt()-1;
-     
-     connectFour.displayBoard(connectFour.setPiece(board, row, column));
-     connectFour.winner(board);
+       System.out.println("Enter the column where you want to make your move.");
+       int column = tu.nextInt()-1;
+       board = connect.setPiece(board, column);       
+       connect.displayBoard(board);
+       connect.winner(board);
      }
-     }
+  }
 
+  
 }
