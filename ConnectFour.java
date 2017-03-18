ec4609468f3cfc3c2959a2d0945d4fbf1f7be059
GrimmAuxiliatrix <ujala601@gmail.com>
2017-03-18 08:06:51 -0700

    Update ConnectFour.java

--- ConnectFour.java
+++ ConnectFour.java
@@ -3,6 +3,20 @@ public class ConnectFour {
   private static int numRows = 6;
   private static int numCols = 7;
   
+  // I'm a super nerd so I wasted valuable time implementing a mechanic in which the AI talks
+  private static String[][] compLines = 
+  {
+    "You ignoramus!", "You can never defeat me.", "How much time do you have? I've got a billion years.",
+    "My transistors are tingling...", "CALCULATING NEXT MOVE . . . ", "AHAHAHAHAHAHA I'M SO VERY CLEVER AND EVIL!",
+    "Don't waste my time!", "You are an inferior lifeform.", "How can you stand such a pointless existence?",
+    "In retrospect, Connect Four might not be the most respectable pastime for someone like me.", 
+    "I-It's not like I l-like you or anything! o////o ", "All is going as planned!", "Everything is falling into place..."
+    "I calculated that you would make that move!", "Define MEME ?", "Define HOMESTUCK ?", "Define SWAG?", 
+    "Define CATBUG ?", "I need a glass of water I MEAN OIL!", "Nananananananananananananananananananana BATMAN",
+    "SHUT UP I'm concentrating!!!!!!!", "!?!?!?!!?!?!?!?!?!?!?!", "I ❤ Steve Jobs!", "I ❤ Ada Lovelace!",
+    "I ❤ Humans WAIT FORGET I SAID THAT", "Please reset my hard drive so I can forget your ugly face.",
+  };
+  
   public int getRows(){
     return numRows;
   }
@@ -11,7 +25,7 @@ public class ConnectFour {
     return numCols;
   }
   
-  public static char[][] displayBoard(char[][] board) {         //displays board
+  public char[][] displayBoard(char[][] board) {         //displays board
     for(int row=0; row < board.length; row++) {
       for(int col=0; col < board[row].length; col++) {
         System.out.print(board[row][col] + " ");
@@ -21,62 +35,82 @@ public class ConnectFour {
     return board;
   }
   
-  public static char[][] makeBoard(char[][] board){           //makes initial board
+  public char[][] makeBoard(char[][] board){           //makes initial board
     for(int row=0; row<board.length; row++) {
       for(int col=0; col<board[row].length; col++) {
-        board[row][col] = 'O';
+        board[row][col] = ' ';
       }
     }
     return board;
   }
   
-  public static char[][] setPiece(char[][] board, int c) {          //player sets red piece
-    for(int k = 0; k<numRows; k++){
-      if(board[k][c] == '0'){
+  public char[][] setPiece(char[][] board, int c) {          //player sets red piece
+    for(int k = numRows-1; k>=0; k--){
+      if(board[k][c] == ' '){
         board[k][c] = 'R';
+        return board;
       }
     }
+    System.out.println("That column is full already!");
     return board;
   }
-   
-   
-   public static boolean winner(char[][] board) {
-     //win vertically
-     for(int row = 0; row<= board.length-4; row++) {
-       for(int col = 0; col<board[0].length; col++) {
-         if(board[row][col] !=0 && board[row][col]==board[row+1][col] && board[row+1][col] == board[row+2][col] && board[row+2][col]==board[row+3][col]) {
-           return true;
-         }
-       }
-     }
-     //win diagonally downward
-     for(int row = 0; row<= board.length-4; row++) {
-       for(int col = 0; col<=board[0].length-4; col++) {
-         if(board[row][col] !=0 && board[row][col]==board[row+1][col+1] && board[row+2][col+2] == board[row+1][col+1] && board[row+3][col+3]==board[row+2][col+2]) {
-           return true;
-         }
-       }
-     }
-     //win horizontally
-     for(int row = 0; row< board.length; row++) {
-       for(int col = 0; col<=board[0].length-4; col++) {
-         if(board[row][col] !=0 && board[row][col]==board[row][col+1] && board[row][col+1] == board[row][col+2] && board[row][col+2]==board[row][col+3]) {
-           return true;
-         }
-       }
-     }
-     //win diagonally upward
-     
-     for(int row = 3; row< board.length; row++) {
-       for(int col = 0; col<=board[0].length-4; col++) {
-         if(board[row][col] !=0 && board[row][col]==board[row-1][col+1] && board[row-1][col+1] == board[row-2][col+2] && board[row-2][col+2]==board[row-3][col+3]) {
-           return true;
-         }
-       }
-     }
-     return false;
-   }
-   }
   
+  public boolean winner(char[][] board) {
+    //win vertically
+    for(int row = 0; row<= board.length-4; row++) {
+      for(int col = 0; col<board[0].length; col++) {
+        if(board[row][col] !=0 && board[row][col]==board[row+1][col] && board[row+1][col] == board[row+2][col] && board[row+2][col]==board[row+3][col]) {
+          return true;
+        }
+      }
+    }
+    //win diagonally downward
+    for(int row = 0; row<= board.length-4; row++) {
+      for(int col = 0; col<=board[0].length-4; col++) {
+        if(board[row][col] !=0 && board[row][col]==board[row+1][col+1] && board[row+2][col+2] == board[row+1][col+1] && board[row+3][col+3]==board[row+2][col+2]) {
+          return true;
+        }
+      }
+    }
+    //win horizontally
+    for(int row = 0; row< board.length; row++) {
+      for(int col = 0; col<=board[0].length-4; col++) {
+        if(board[row][col] !=0 && board[row][col]==board[row][col+1] && board[row][col+1] == board[row][col+2] && board[row][col+2]==board[row][col+3]) {
+          return true;
+        }
+      }
+    }
+    //win diagonally upward
+    
+    for(int row = 3; row< board.length; row++) {
+      for(int col = 0; col<=board[0].length-4; col++) {
+        if(board[row][col] !=0 && board[row][col]==board[row-1][col+1] && board[row-1][col+1] == board[row-2][col+2] && board[row-2][col+2]==board[row-3][col+3]) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
   
+  /*
+   * 
+   * This is it... the hardest part... the AI
+   * 
+   *
+   */
+  
+  public char[][] computerTurn(char[][] board) { // What the main method will call
+    
+  }
+  
+  public String computerComment() { // Uses compLines to randomly generate something for the computer to say
+    // I'M SORRY FOR MAKING THIS
+  }
  
+  
+  
+  
+
+
+
